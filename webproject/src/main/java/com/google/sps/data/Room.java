41ad6d3ed81868b8f484a688f6b3bfc3ea930ef9
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2020-07-09 00:07:13 +0800

    Change indentation to 4 spaces

--- webproject/src/main/java/com/google/sps/data/Room.java
+++ webproject/src/main/java/com/google/sps/data/Room.java
@@ -6,59 +6,7 @@ import com.google.appengine.api.datastore.Entity;
  * This class defines room objects
  */
 public class Room {
-  private String id;
-  private String title;
-  private String link;
-  private String description;
-  private int deliveryLocation; //modify to use different representation of location if needed
-  private int phoneNumber; //modify to use different representation of contact details if needed
-  private Category category;
-  private double minPrice;
-
-  private Room (Builder builder) {
-    this.id = builder.id;
-    this.title = builder.title;
-    this.link = builder.link;
-    this.description = builder.description;
-    this.deliveryLocation = builder.deliveryLocation;
-    this.phoneNumber = builder.phoneNumber;
-    this.category = builder.category;
-    this.minPrice = builder.minPrice;
-  }
-
-  public Entity toEntity() {
-    Entity roomEntity = new Entity("Room");
-
-    roomEntity.setProperty("id", id);
-    roomEntity.setProperty("title", title);
-    roomEntity.setProperty("link", link);
-    roomEntity.setProperty("description", description);
-    roomEntity.setProperty("deliveryLocation", deliveryLocation);
-    roomEntity.setProperty("phoneNumber", phoneNumber);
-    roomEntity.setProperty("category", category);
-    roomEntity.setProperty("minPrice", minPrice);
-
-    return roomEntity;
-  }
-
-  public String getId() {return id;}
-
-  public String getTitle() {return title;}
-
-  public String getLink() {return link;}
-
-  public String getDescription() {return description;}
-
-  public int getDeliveryLocation() {return deliveryLocation;}
-
-  public int getPhoneNumber() {return phoneNumber;}
-
-  public Category getCategory() {return category;}
-
-  public double getMinPrice() {return minPrice;}
-
-  public static class Builder {
-    private final String id;
+    private String id;
     private String title;
     private String link;
     private String description;
@@ -67,47 +15,99 @@ public class Room {
     private Category category;
     private double minPrice;
 
-    public Builder(String id) {
-      this.id = id;
+    private Room (Builder builder) {
+        this.id = builder.id;
+        this.title = builder.title;
+        this.link = builder.link;
+        this.description = builder.description;
+        this.deliveryLocation = builder.deliveryLocation;
+        this.phoneNumber = builder.phoneNumber;
+        this.category = builder.category;
+        this.minPrice = builder.minPrice;
     }
 
-    public Builder setTitle(String title) {
-      this.title = title;
-      return this;
+    public Entity toEntity() {
+        Entity roomEntity = new Entity("Room");
+
+        roomEntity.setProperty("id", id);
+        roomEntity.setProperty("title", title);
+        roomEntity.setProperty("link", link);
+        roomEntity.setProperty("description", description);
+        roomEntity.setProperty("deliveryLocation", deliveryLocation);
+        roomEntity.setProperty("phoneNumber", phoneNumber);
+        roomEntity.setProperty("category", category);
+        roomEntity.setProperty("minPrice", minPrice);
+
+        return roomEntity;
     }
 
-    public Builder setLink(String link) {
-      this.link = link;
-      return this;
-    }
+    public String getId() {return id;}
 
-    public Builder setDescription(String description) {
-      this.description = description;
-      return this;
-    }
+    public String getTitle() {return title;}
 
-    public Builder setDeliveryLocation(int deliveryLocation) {
-      this.deliveryLocation = deliveryLocation;
-      return this;
-    }
+    public String getLink() {return link;}
 
-    public Builder setPhoneNumber(int phoneNumber) {
-      this.phoneNumber = phoneNumber;
-      return this;
-    }
+    public String getDescription() {return description;}
 
-    public Builder setCategory(Category category) {
-      this.category = category;
-      return this;
-    }
+    public int getDeliveryLocation() {return deliveryLocation;}
 
-    public Builder setMinPrice(double minPrice) {
-      this.minPrice = minPrice;
-      return this;
-    }
+    public int getPhoneNumber() {return phoneNumber;}
 
-    public Room build() {
-      return new Room(this);
+    public Category getCategory() {return category;}
+
+    public double getMinPrice() {return minPrice;}
+
+    public static class Builder {
+        private final String id;
+        private String title;
+        private String link;
+        private String description;
+        private int deliveryLocation; //modify to use different representation of location if needed
+        private int phoneNumber; //modify to use different representation of contact details if needed
+        private Category category;
+        private double minPrice;
+
+        public Builder(String id) {
+            this.id = id;
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setLink(String link) {
+            this.link = link;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setDeliveryLocation(int deliveryLocation) {
+            this.deliveryLocation = deliveryLocation;
+            return this;
+        }
+
+        public Builder setPhoneNumber(int phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder setCategory(Category category) {
+            this.category = category;
+            return this;
+        }
+
+        public Builder setMinPrice(double minPrice) {
+            this.minPrice = minPrice;
+            return this;
+        }
+
+        public Room build() {
+            return new Room(this);
+        }
     }
-  }
 }
