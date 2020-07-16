f7a6176e51c8967eb773f68d7bd0201064039115
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2020-07-16 23:53:33 +0800

    Create OrderServlet to post new orders to firebase

---
+++ webproject/src/main/java/com/google/sps/servlets/OrderServlet.java
@@ -0,0 +1,63 @@
+package com.google.sps.servlets;
+
+import com.google.gson.Gson;
+import com.google.sps.data.Order;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@WebServlet("/order")
+public class OrderServlet extends HttpServlet {
+    private static Gson gson = new Gson();
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Order newOrder = Order.newBuilder()
+                .setUser(""/*add get username here*/)
+                .setProduct(request.getParameter("product"))
+                .setQuantity(Integer.parseInt(request.getParameter("quantity")))
+                .setUnitPrice(Double.parseDouble(request.getParameter("unitPrice")))
+                .build();
+
+        String roomId = request.getParameter("roomId");
+        String urlString = "https://summer20-sps-47.firebaseio.com/rooms/" + roomId + "/orders.json";
+
+        URL url = new URL(urlString);
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+
+        String orderJson = gson.toJson(newOrder);
+
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = orderJson.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        String jsonResponse = "";
+
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder firebaseResponse = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                firebaseResponse.append(responseLine.trim());
+            }
+
+            jsonResponse = firebaseResponse.toString();
+        }
+
+        response.setContentType("text/json; charset=UTF-8");
+        response.getWriter().print(jsonResponse);
+    }
+}
