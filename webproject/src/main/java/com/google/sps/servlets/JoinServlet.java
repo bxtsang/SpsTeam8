0243aa4e7ae6870c8620462241084edac02a7a7c
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2020-07-19 01:58:27 +0800

    Add comment for future change

--- webproject/src/main/java/com/google/sps/servlets/JoinServlet.java
+++ webproject/src/main/java/com/google/sps/servlets/JoinServlet.java
@@ -62,6 +62,7 @@ public class JoinServlet extends HttpServlet {
 
         String url = "https://summer20-sps-47.firebaseio.com/UserRoom.json?orderBy=%22userEmailRoom%22&equalsTo=%22" + userEmail + "_" + roomId + "%22";
 
+        //Use Firebase class once merged
         HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("Accept", "application/json");
