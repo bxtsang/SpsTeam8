4ba9fab44f61e71cddbf01827957fe4ad9a4acad
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2020-07-19 02:11:47 +0800

    Change getRooms function to getMyRooms and get rooms which user is in from /myRooms

--- webproject/src/main/webapp/js/listings.js
+++ webproject/src/main/webapp/js/listings.js
@@ -113,21 +113,44 @@ function getSearchResults() {
   }
 }
 
-function getRooms() {
-  document.getElementById("listings-card-container").innerHTML = '';
-  firebase.database().ref("rooms/").once('value', function(snapshot) {
-    snapshot.forEach(function(child) { 
-      var snap = child.val();
-      document.getElementById("listings-card-container").innerHTML += `
-    <a href="./room.html?${child.key}">
-      <div class="shadow-sm p-3 mb-5 bg-white rounded listing-card">
-        <div class="row form-group">
-          <div class="col-md-6 mb-3 mb-md-0">
-            <span class="card-heading shop-name">${snap.title}</span>
-          </div>
+async function getMyRooms() {
+  var response = await fetch("/myRooms");
+  var responseJson = await response.json();
+  var entries = Object.entries(responseJson)
+
+  let childHtmlString = "";
+
+  entries.forEach(room => {
+    var roomData = room[1];
+
+    childHtmlString += `<div class="shadow-sm p-3 mb-5 bg-white rounded listing-card">
+      <div class="row form-group">
+        <div class="col-md-6 mb-3 mb-md-0">
+          <span class="card-heading title"><a href="./room.html?${room[0]}>${roomData.title}</a></span>
+          <br />
+          <span class="card-field">Postal Code: </span>
+          <span class="card-value postal-code-value">
+            ${roomData.deliveryLocation}
+          </span>
+          <br />
+          <span class="card-description">
+            ${roomData.description}
+          </span>
+        </div>
+        <div class="col-md-6 text-right">
+          <span class="card-field">Category: </span>
+          <span class="card-value category-value">${roomData.category}</span>
+          <br />
+          <span class="card-field">Delivery fee: </span>
+          <span class="card-value">$${roomData.deliveryFee}</span>
+          <br />
+          <span class="card-field">$ current orders value: </span>
+          <span class="card-value">$${roomData.ordersValue}</span>
+          <br />
         </div>
       </div>
-    </a>`;
-    });
-  });
+    </div>`;
+  })
+
+  document.getElementById("listings-card-container").innerHTML = childHtmlString;
 }
