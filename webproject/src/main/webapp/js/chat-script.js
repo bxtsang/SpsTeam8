1a9c8ca20f62fa0af649cf2b47a469ceac2813af
arjavibahety <arjavibahety@gmail.com>
2020-07-19 02:04:10 +0800

    WIP

--- webproject/src/main/webapp/js/chat-script.js
+++ webproject/src/main/webapp/js/chat-script.js
@@ -1,22 +1,9 @@
 var roomId = window.location.search.substr(1);
 window.onload = function() {
-        roomID = window.location.search.substr(1);
-        getRoomDetails();
-        // document.getElementById('chat').innerHTML = '<iframe width="100%" height="100%" frameborder="0" src="chat.html?' + roomID + '"></iframe>';
-    }
-    // async function getRoom() {
-    //     var response = await fetch(`https://summer20-sps-47.firebaseio.com/rooms/${roomId}.json`);
-    //     var room = await response.json();
-    //     console.log("Room: ", room);
-    //     var roomKeys = Object.keys(room);
-    //     roomKeys.forEach(key => {
-    //         document.querySelector("#room-details").innerHTML += `
-    //     <tr>
-    //       <td>${key}</td>
-    //       <td>${room[key]}</td>
-    //     </tr>`;
-    //     });
-    // }
+    roomID = window.location.search.substr(1);
+    getRoomDetails();
+    document.getElementById('chat-box').innerHTML = '<iframe width="100%" height="100%" frameborder="0" src="chat.html?' + roomID + '"></iframe>';
+}
 $(document).ready(function() {
     database = firebase.database();
     name = "user " + Math.floor(Math.random() * Math.floor(5));
