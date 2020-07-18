5d414068ccd6c13f68190880e8b5c29dada01aa3
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2020-07-19 00:44:19 +0800

    Add doGet to check if user has joined room already

--- webproject/src/main/java/com/google/sps/servlets/JoinServlet.java
+++ webproject/src/main/java/com/google/sps/servlets/JoinServlet.java
@@ -8,14 +8,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.sps.authentication.AuthenticationHandler;
 import com.google.sps.data.Message;
 import com.google.sps.data.UserRoom;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @WebServlet("/join")
 public class JoinServlet extends HttpServlet {
@@ -53,5 +52,39 @@ public class JoinServlet extends HttpServlet {
 
         response.setStatus(200);
         response.getWriter().println(roomId);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String roomId = request.getParameter("roomId");
+        User user = AuthenticationHandler.getCurrentUser();
+        String userEmail = user.getEmail();
+
+        String url = "https://summer20-sps-47.firebaseio.com/UserRoom.json?orderBy=%22userEmailRoom%22&equalsTo=%22" + userEmail + "_" + roomId + "%22";
+
+        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+
+        String jsonResponse = "";
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            StringBuilder firebaseResponse = new StringBuilder();
+            String line;
+
+            while ((line = in.readLine()) != null) {
+                firebaseResponse.append(line);
+            }
+
+            jsonResponse = firebaseResponse.toString();
+        }
+
+        response.setContentType("text/plain");
+
+        if (jsonResponse.substring(0, 2) == "{}") {
+            response.getWriter().print("Join");
+        } else {
+            response.getWriter().print("Chat");
+        }
     }
 }
