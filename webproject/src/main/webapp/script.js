babeec7f4c969a57a77c226e1750a9d20ab58ab6
Kevin Lim <lyskevin@gmail.com>
2020-07-18 23:14:14 +0800

    Add function to append username to logout button

--- webproject/src/main/webapp/script.js
+++ webproject/src/main/webapp/script.js
@@ -15,3 +15,13 @@ function logout() {
         window.location.href = response;
     });
 }
+
+/**
+ * Appends the user's username to the logout button.
+ */
+function appendUsernameToLogoutButton() {
+    fetch('/username').then(response => response.text()).then(response => {
+        const textToAppend = ' (' + response + ')';
+        document.getElementById('logout-button').insertAdjacentHTML('beforeend', textToAppend);
+    });
+}
