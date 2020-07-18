a200fcc781801058b4a73e19d01166a1632cc84b
arjavibahety <arjavibahety@gmail.com>
2020-07-19 00:28:23 +0800

    Setup firebase

--- webproject/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ webproject/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -29,7 +29,7 @@ public class FormHandlerServlet extends HttpServlet {
   public void init() {
       try {
       // Fetch the service account key JSON file contents
-        FileInputStream serviceAccount = new FileInputStream("/home/mtang/cloudshell_open/SpsTeam8-0/webproject/src/main/java/com/google/sps/servlets/key.json");
+        FileInputStream serviceAccount = new FileInputStream("key.json");
 
         // Initialize the app with a service account, granting admin privileges
         FirebaseOptions options = new FirebaseOptions.Builder()
