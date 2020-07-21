52c7a1b70d34b7777debedd143ff406a7896e8bc
Michaela <michaelatang99@gmail.com>
2020-07-21 16:30:29 +0800

    Add header links to Rooms

--- webproject/src/main/webapp/js/chat-room.js
+++ webproject/src/main/webapp/js/chat-room.js
@@ -2,6 +2,7 @@ var roomID = window.location.search.substr(1);
 window.onload = function() {
     roomID = window.location.search.substr(1);
     getRoomDetails(roomID);
+    getHeaderLinks();
     document.getElementById('chat-box').innerHTML = '<iframe width="100%" height="100%" frameborder="0" src="chat.html?' + roomID + '"></iframe>';
 }
 
@@ -16,3 +17,9 @@ function fetchBlobstoreUrl() {
             messageForm.classList.remove('hidden');
         });
 }
+
+function getHeaderLinks() {
+    document.getElementById('my-order-link').href = '/roomMyOrder.html?' + roomID;
+    document.getElementById('all-orders-link').href = '/roomAllOrders.html?' + roomID;
+}
+
