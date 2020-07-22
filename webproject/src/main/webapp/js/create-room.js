5afc87773efed4939c027dd057320d49dbd13d64
arjavibahety <arjavibahety@gmail.com>
2020-07-22 16:57:16 +0800

    Select button WIP

---
+++ webproject/src/main/webapp/js/create-room.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    getCategories();
+}
+
+function getCategories() {
+    let categoryBox = document.getElementById("category");
+    let categories = ["Chinese", "Thai", "Indian", "Western", "Halal"]
+    for (let category of categories) {
+        categoryBox.innerHTML += `<option value="${category.toUpperCase()}">${category}</option>`
+    }
+}
