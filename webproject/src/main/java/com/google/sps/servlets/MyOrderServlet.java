dfc0548102e10004c43fc3d6d9d8d1f75c212946
Michaela <michaelatang99@gmail.com>
2020-08-03 08:40:44 +0000

    Add validity check for orderID

--- webproject/src/main/java/com/google/sps/servlets/MyOrderServlet.java
+++ webproject/src/main/java/com/google/sps/servlets/MyOrderServlet.java
@@ -17,6 +17,7 @@ import java.net.URL;
 import java.io.OutputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 import static javax.swing.JOptionPane.showMessageDialog;
 
@@ -37,12 +38,12 @@ public class MyOrderServlet extends HttpServlet {
     public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
         BufferedReader reader = request.getReader();
         String orderId = reader.readLine();
-        if (orderId == null) {
-            showMessageDialog(null, "orderID not found");        
-        }
         orderId = orderId.split("=")[1];
-        String firebaseResponse = Firebase.sendRequest("https://summer20-sps-47.firebaseio.com/orders/" + orderId + ".json", "DELETE", orderId);
-        response.setStatus(200);
-        response.getWriter().print(firebaseResponse);
+        String orderData = Firebase.sendGetRequest("https://summer20-sps-47.firebaseio.com/orders/" + orderId + ".json");
+        if (!Objects.equals(orderData, "null")) {
+            String firebaseResponse = Firebase.sendRequest("https://summer20-sps-47.firebaseio.com/orders/" + orderId + ".json", "DELETE", orderId);
+            response.setStatus(200);
+            response.getWriter().print(firebaseResponse);
+        }
     }
 }
