12a71221644524da0b39aa44036998d2cdc47bf6
Kevin Lim <lyskevin@gmail.com>
2020-08-07 20:43:09 +0800

    Update CloseRoomServiceImpl to use Room class

--- webproject/src/main/java/com/google/sps/services/implementations/CloseRoomServiceImpl.java
+++ webproject/src/main/java/com/google/sps/services/implementations/CloseRoomServiceImpl.java
@@ -1,7 +1,5 @@
 package com.google.sps.services.implementations;
 
-
-import com.google.sps.data.RoomProto.Room;
 import com.google.sps.dataManagers.RoomManager;
 import com.google.sps.proto.CloseRoomProto.CloseRoomResponse;
 import com.google.sps.proto.CloseRoomProto.CloseRoomRequest;
@@ -11,16 +9,17 @@ import javax.inject.Inject;
 import javax.servlet.ServletException;
 
 public class CloseRoomServiceImpl implements CloseRoomService {
-    private UserRoomManager userRoomManager;
+    private RoomManager roomManager;
 
     @Inject
-    public CloseRoomServiceImpl(UserRoomManager userRoomManager) {
-        this.userRoomManager = userRoomManager;
+    public CloseRoomServiceImpl(RoomManager userRoomManager) {
+        this.roomManager = roomManager;
     }
 
     @Override
     public CloseRoomResponse execute(CloseRoomRequest closeRoomRequest) throws ServletException {
-        Room room = roomManager.closeRoom(closeRoomRequest.getRoomId());
+        String roomId = closeRoomRequest.getRoomId();
+        roomManager.closeRoom(roomId);
         return CloseRoomResponse.newBuilder()
                 .setRoomId(closeRoomRequest.getRoomId())
                 .build();
