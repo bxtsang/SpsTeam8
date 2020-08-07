f99f4b39954951d33ecf4d75019eaae93dbd3a53
Kevin Lim <lyskevin@gmail.com>
2020-08-07 14:20:48 +0800

    Update doPost in CloseRoomServlet to block until the Firebase database is updated before redirecting to the entry page

--- webproject/src/main/java/com/google/sps/servlets/CloseRoomServlet.java
+++ webproject/src/main/java/com/google/sps/servlets/CloseRoomServlet.java
@@ -7,18 +7,23 @@ import com.google.sps.authentication.AuthenticationHandler;
 import com.google.sps.util.FirebaseUtil;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
 
 /**
  * A servlet which manages room closure.
  */
 public class CloseRoomServlet extends HttpServlet {
     private static Gson gson = new Gson();
+    private CountDownLatch countDownLatch;
     private FirebaseUtil firebaseUtil;
 
     public CloseRoomServlet() throws Exception {
+        countDownLatch = new CountDownLatch(1);
         firebaseUtil = new FirebaseUtil();
     }
 
@@ -27,14 +32,20 @@ public class CloseRoomServlet extends HttpServlet {
      * @param request An HttpServletRequest object that contains the request the client has made of the servlet.
      * @param response An HttpServletResponse object that contains the response the servlet sends to the client.
      * @throws IOException If an input or output error is detected when the servlet handles the server's request.
+     * @throws ServletException If there is an error updating the database when closing a room.
      */
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String roomId = request.getParameter("roomId");
         firebaseUtil.getRoomsReference().child(roomId).child("isOpen")
-                .setValue(Boolean.FALSE, (databaseError, databaseReference) -> {});
+                .setValue(Boolean.FALSE, (error, reference) -> countDownLatch.countDown());
 
-        // Send redirect here instead of inside the lambda expression due to possibility of IOException
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new ServletException("There was an error updating the database when closing the room.");
+        }
+        
         response.sendRedirect("/");
     }
 }
