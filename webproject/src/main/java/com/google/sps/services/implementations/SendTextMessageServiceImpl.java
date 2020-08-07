8dfbc59ba600ab710ee8d3cd0775ef614992c2b5
arjavibahety <arjavibahety@gmail.com>
2020-08-08 03:04:40 +0800

    Fix vertical alignment

--- webproject/src/main/java/com/google/sps/services/implementations/SendTextMessageServiceImpl.java
+++ webproject/src/main/java/com/google/sps/services/implementations/SendTextMessageServiceImpl.java
@@ -31,6 +31,5 @@ public class SendTextMessageServiceImpl implements SendTextMessageService {
         Message messageObject = messagesManager.addMessage(user, message, roomId);
 
         return SendTextMessageResponse.newBuilder().setMessages(messageObject).build();
-
     }
 }
