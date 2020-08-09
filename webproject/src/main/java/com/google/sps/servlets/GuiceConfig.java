75118729bf06a05e5073b4eeb9e98b971cab0e7e
Kevin Lim <lyskevin@gmail.com>
2020-08-09 17:02:34 +0000

    Update UsernameServlet to use DI

--- webproject/src/main/java/com/google/sps/servlets/GuiceConfig.java
+++ webproject/src/main/java/com/google/sps/servlets/GuiceConfig.java
@@ -8,9 +8,11 @@ import com.google.inject.servlet.ServletModule;
 
 import com.google.sps.services.implementations.CloseRoomServiceImpl;
 import com.google.sps.services.implementations.UserRoomStatusServiceImpl;
+import com.google.sps.services.implementations.UsernameServiceImpl;
 import com.google.sps.services.implementations.JoinRoomServiceImpl;
 import com.google.sps.services.interfaces.CloseRoomService;
 import com.google.sps.services.interfaces.UserRoomStatusService;
+import com.google.sps.services.interfaces.UsernameService;
 import com.google.sps.services.interfaces.JoinRoomService;
 
 public class GuiceConfig extends GuiceServletContextListener {
@@ -25,11 +27,13 @@ public class GuiceConfig extends GuiceServletContextListener {
                 bind(CloseRoomService.class).to(CloseRoomServiceImpl.class);
                 bind(JoinRoomService.class).to(JoinRoomServiceImpl.class);
                 bind(UserRoomStatusService.class).to(UserRoomStatusServiceImpl.class);
+                bind(UsernameService.class).to(UsernameServiceImpl.class);
 
                 serve("/landing").with(LandingServlet.class);
                 serve("/joinRoom").with(JoinRoomServlet.class);
                 serve("/userRoomStatus").with(UserRoomStatusServlet.class);
                 serve("/closeRoom").with(CloseRoomServlet.class);
+                serve("username").with(UsernameServlet.class);
             }
         });
     }
