10f1e350c0d8b3916c4c97d951f998ca1431bbba
arjavibahety <arjavibahety@gmail.com>
2020-08-10 03:45:54 +0800

    Change RoomsListingComparator formula to calculate the estimated number of people needed to hit the minPrice

--- webproject/src/main/java/com/google/sps/Comparators/RoomsListingComparator.java
+++ webproject/src/main/java/com/google/sps/Comparators/RoomsListingComparator.java
@@ -6,21 +6,23 @@ import com.google.sps.data.RoomProto.Room;
 
 /**
  * A comparator to sort rooms based on how full they are.
- * The "full" value is calculated as follows:
- * 1. The difference of minPrice and ordersValue gives the order amount needed to hit the minPrice.
- * 2. The delivery fee is added to the difference because it is independent of the minPrice and ordersValue.
- * 3. The value calculated in the previous step is divided by the no of users in a room to obtain the average per
- * person order value.
- * A smaller "full" value means that the average per person order value is smaller. This may approximately mean that
- * the room is closer to hitting the minPrice / is very active, thus the order by the users maybe be placed soon.
- * Therefore, it will be listed before rooms with a larger "full" value.
+ * The estimated number of people needed to hit the minPrice is calculated as follows:
+ * 1. The average per person order cost is calculated which is inclusive of the delivery fee.
+ * 2. The difference to minPrice order is calculated and divided by the average per person order cost calculated in
+ * the previous step. This gives the estimated additional no of people needed to hit the minPrice.
+ * A smaller value means that lesser number of people are needed to hit the minPrice.
+ * Therefore, it will be listed before rooms with a larger value.
  */
 public class RoomsListingComparator implements Comparator<Room> {
     public int compare(Room aRoom, Room bRoom) {
         double aRoomAveragePerPersonValue =
-                (aRoom.getMinPrice() - aRoom.getOrdersValue() + aRoom.getDeliveryFee()) / aRoom.getUsersCount();
+                (aRoom.getOrdersValue() + aRoom.getDeliveryFee()) / aRoom.getUsersCount();
+        double aEstimatedNoOfPeopleNeeded = (aRoom.getMinPrice() - aRoom.getOrdersValue()) / aRoomAveragePerPersonValue;
+
         double bRoomAveragePerPersonValue =
-                (bRoom.getMinPrice()  - bRoom.getOrdersValue() + bRoom.getOrdersValue()) / bRoom.getUsersCount();
-        return  Double.compare(aRoomAveragePerPersonValue, bRoomAveragePerPersonValue);
+                (bRoom.getOrdersValue() + bRoom.getOrdersValue()) / bRoom.getUsersCount();
+        double bEstimatedNoOfPeopleNeeded = (bRoom.getMinPrice() - bRoom.getOrdersValue()) / bRoomAveragePerPersonValue;
+
+        return  Double.compare(aEstimatedNoOfPeopleNeeded, bEstimatedNoOfPeopleNeeded);
     }
 }
